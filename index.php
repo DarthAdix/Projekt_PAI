830c1770e61e8496ef06c1707666f46b9d3b425c
DarthAdix <56804504+DarthAdix@users.noreply.github.com>
2019-11-24 02:12:40 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="pl" id="top">
+<head>
+  <title>Strzelnica Gdańsk</title>
+  <?php include 'header.html' ?>
+</head>
+<body>
+
+<?php include 'navbar.php' ?>
+
+<div class="jumbotron text-center">
+    <img src="logo3.png" alt="SG" width="200">
+  <h1>Strzelnica Gdańsk</h1> 
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-8">
+      <h2>WITAMY NA STRONIE STRZELNICY GDAŃSK</h2><br>
+      <h4><strong>Strzelnica Gdańsk</strong> posiada jeden z największych arsenałów broni w Polsce. Istniejemy od asiao sifoab oauso awho ashdo ashdo asdh aoshdoaishoad </h4><br>
+    </div>
+    <div class="col-sm-4">
+      <img src="rifle.png" width="100%">
+    </div>
+  </div>
+</div>
+  <div class="container text-center">
+    <div id="carousel-gallery" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carousel-gallery" data-slide-to="0" class="active"></li>
+      <li data-target="#carousel-gallery" data-slide-to="1"></li>
+      <li data-target="#carousel-gallery" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="falcon1.jpg" style="height: 500px;" alt=".1.">
+      </div>
+      <div class="carousel-item">
+        <img src="falcon2.jpg" style="height: 500px;" alt=".2.">
+      </div>
+      <div class="carousel-item">
+        <img src="ss.jpg" style="height: 500px;" alt=".3.">
+      </div>
+      <a class="carousel-control-prev" href="#carousel-gallery" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#carousel-gallery" role="button" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+    </div>
+    
+  </div>
+</div>
+
+
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>About Me</h2>
+      <h5>Photo of me:</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+      <h3>Some Links</h3>
+      <p>Lorem ipsum dolor sit ame.</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-8">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Dec 7, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <br>
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+    <div class="col-sm-8">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Dec 7, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <br>
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+    <div class="col-sm-8">
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Dec 7, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <br>
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+  </div>
+</div>
+
+<footer class="container-fluid text-center">
+  <a href="#top" title="To Top">
+    <span class="fa fa-angle-up fa-3x"></span>
+  </a>
+  <p>Copyright &copy <a href="mailto: admin@strzelectwo.cba.pl" style="color: #fff;">Adam Kowalczyk</a> 2019</p>
+</footer>
+
+</body>
---
+++ navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="index.php"><img src="logo3.png" width="40"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav" style="margin-right: auto">
+      <li class="nav-item">
+        <a class="nav-link" href="oferta.php">Oferta</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="bronie.php">Uzbrojenie</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="cennik.php">Cennik</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="kontakt.php">Kontakt</a>
+      </li>    
+    </ul>
+    <ul class="navbar-nav navbar-right">
+      <li class="nav-item"><a class="nav-link" href="#"><span class="fa fa-angle-right"></span> Login</a></li>
+    </ul>
+  </div>  
+</nav>
